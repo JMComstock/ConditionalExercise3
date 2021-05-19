3dc9d1741b180c68b333ea9ff6de80bf1fd5736d
Jason Comstock <71102533+JMComstock@users.noreply.github.com>
2021-05-19 15:10:16 -0400

    Add files via upload

---
+++ ConditionalExercise3/ConditionalExercise3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConditionalExercise3
+{
+    class Program
+    {
+        // Write a program and ask the user to enter the width and height of an image. Then tell if the image 
+        /// is landscape or portrait.
+        static void Main(string[] args)
+        {
+            Console.Write("Image width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Image height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
+            Console.WriteLine("Image orientation is " + orientation);
+
+        }
+        public enum ImageOrientation
+        {
+            Landscape,
+            Portrait
+        }
+    }
+}
